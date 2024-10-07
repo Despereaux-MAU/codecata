2f12f91ddf4789c9b964b00a1b2d9d1f4b82779a
despereaux <157133321+Despereaux-MAU@users.noreply.github.com>
2024-10-07 12:03:44 +0900

    16번 문제 clear

--- src/Solution.java
+++ src/Solution.java
@@ -1,12 +1,10 @@
 class Solution {
-    public int solution(int n) {
-        int x = 0;
-        for (int i = 2 ; i < n ; i++) {
-            if (n % i == 1) {
-                x = i;
-                break;
-            }
+    // 함수 solution은 정수 x와 자연수 n을 입력 받아, x부터 시작해 x씩 증가하는 숫자를 n개 지니는 리스트를 리턴해야 합니다.
+    public long[] solution(long x, int n) {
+        long[] answer = new long[n];
+        for (int i = 0 ; i < n ; i++) {
+            answer[i] = x * (i + 1);
         }
-        return x;
+        return answer;
     }
 }
